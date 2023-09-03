bf0eee172e2b3db3427613f0de591ab5ff245e48
tomhoon <gnsdl9108@naver.com>
2023-09-03 22:42:51 +0900

    axios 추가, 운영,개발 api 분리

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false
+  lintOnSave:false,
+  devServer: {
+    // 💕운영
+    proxy: 'http://oliveyoungproject.shop' 
+
+    // 💕개발
+    // proxy: 'http://localhost:8082' 
+  }
 })
