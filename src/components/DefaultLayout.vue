43f7f423202fca0afbdafb4209d44065a6265f02
김윤경 <kim-yoonkyung@gim-yungyeong-ui-MacBookAir.local>
2023-09-03 20:21:29 +0900

    first commit

---
+++ src/components/DefaultLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h3>This is DefaultLayout</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import DefaultLayout from '@/components/DefaultLayout.vue';
 
-createApp(App).mount('#app')
+import {createWebHistory, createRouter} from 'vue-router'
+
+const routes = [
+    {
+        path: '/',
+        component: DefaultLayout
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+const app = createApp(App);
+
+app.component('DefaultLayout', DefaultLayout);
+
+app.use(router);
+
+app.mount('#app');
