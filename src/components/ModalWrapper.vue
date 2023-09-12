5bca21768948232ff5745c5fd951aa55dfb9b21d
cet4713@gmail.com <cet4713@gmail.com>
2023-09-12 19:35:31 +0900

    Modal Test

---
+++ src/components/ModalWrapper.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="modal" v-if="isOpen" :style="{ width: width, height: height }">
+    <div class="modal-content">
+      <slot></slot> <!-- 모달 내용을 slot으로 삽입합니다. -->
+      <button @click="closeModal">모달 닫기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    width: String, // 너비를 문자열로 전달 받음
+    height: String, // 높이를 문자열로 전달 받음
+  },
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    modalOpen() {
+      this.isOpen = true;
+    },
+    closeModal() {
+      this.isOpen = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* 모달 스타일링 */
+.modal {
+  /* 스타일링 내용 추가 */
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* 모달 배경의 반투명 검은색 배경 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999; /* 다른 요소 위에 표시 */
+}
+
+.modal-content {
+  /* 스타일링 내용 추가 */
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2); /* 그림자 효과 */
+}
+</style>
--- src/components/TestStack.vue
+++ src/components/TestStack.vue
@@ -1,14 +1,30 @@
 <template>
   <div> 테스트 스택.vue </div>
+  <button @click="modalOpen">모달 열기</button>
+
+  <modalWrapper ref="modal" :width="200" :height="500">
+      <!-- 모달에 들어갈 내용을 이 부분에 추가 -->
+      <h2>모달 제목</h2>
+      <p>모달 내용을 여기에 추가합니다.</p>
+  </modalWrapper>
+
   <button @click="backPage">뒤로가기</button>
 </template>
 
 <script>
+import ModalWrapper from '@/components/ModalWrapper.vue';
+
 export default {
+    components:{
+      ModalWrapper,  
+    },
     props: ['pageParams'],
     methods:{
         backPage() {
             this.$backPage({boardParam: '백페이지에서 사용할 파람'})
+        },
+        modalOpen() {
+            this.$refs.modal.modalOpen();
         }
     }
 }
