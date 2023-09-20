5f8de5a084a2a9b95e70631b61d5d05550c0e9f6
cet4713@gmail.com <cet4713@gmail.com>
2023-09-20 23:02:18 +0900

    StackLayer 폴더화

--- src/components/StackLayer/TestStack.vue
+++ src/components/StackLayer/TestStack.vue
@@ -1,5 +1,5 @@
 <template>
-  <div> 테스트 스택.vue </div>
+  <div> 테스트 스택.vue // NewStackLayer.vue 에서 받아온 데이터 {{ transferObj.backData }} </div>
   <button @click="modalOpen">모달 열기</button>
 
   <modalWrapper ref="modal" :width="500" :height="500" :clickToClose=true>
@@ -9,6 +9,10 @@
   </modalWrapper>
 
   <button @click="backPage">뒤로가기</button>
+
+  <button @click="goNewStackLayer(2)">NewStackLayer</button>
+
+
 </template>
 
 <script>
@@ -18,13 +22,16 @@ export default {
     components:{
       ModalWrapper,  
     },
-    props: ['pageParams'],
+    props: ['pageParams', 'transferObj'],
     methods:{
         backPage() {
             this.$backPage({boardParam: '백페이지에서 사용할 파람'})
         },
         modalOpen() {
             this.$refs.modal.modalOpen();
+        },
+        goNewStackLayer(payload) {
+            this.$pushContents('NewStackLayer', {add : payload});
         }
     }
 }
