37b69f2277a4d92b40a4aa20dacddd1b4a7f5f9d
git l10000@l10000s.cafe24app.com:l10000_l10000s <kmi5063@naver.com>
2023-09-24 16:41:12 +0900

    마이페이지 진행중

--- src/components/member/Mypage.vue
+++ src/components/member/Mypage.vue
@@ -60,7 +60,53 @@
         </div>
         <!-- GNB끝 -->
         <!--mypage 시작-->
+        <div class="mypage_wrap">
+          <div class="mypage_container">
+            <!--인사 시작-->
+            <div class="intro_wrap">
+              <div class="intro_content">
+                <div class="intro_tit">
+                  <span>
+                    <img src="@/assets/profile_Img.jpg" class="mypage_profile" alt="프로필사진">
+                  </span>
+                  <span>BABY OLIVE 홍길동님 반갑습니다.</span>
+                  <span>
+                    <span>올리브 멤버스 라운지<span> &gt; </span></span>
+                    <span>나의 프로필<span> &gt; </span></span>
+                  </span>
+                </div>
+              </div>
+              <div class="intro_point">
+                <div class="point-box">
+                  <span class="point-box_point">CJ ONE 포인트 <span> 1000 </span> P</span>
+                  <span class="point-box_coupon">쿠폰 <span> 0 </span> 개</span>
+                  <span class="point-box_deposit">예치금 <span> 0 </span> 원</span>
+                </div>
+              </div>
+            </div>
+            <!--인사 끝-->
+            <!--주문/배송조회 시작-->
+            <div>
 
+            </div>
+            <!--주문/배송조회 끝-->
+            <!--좋아요 시작-->
+            <div>
+
+            </div>
+            <!--좋아요 끝-->
+            <!--1:1문의내역 시작-->
+            <div>
+
+            </div>
+            <!--1:1문의내역 끝-->
+            <!--상품Q&A내역 시작-->
+            <div>
+
+            </div>
+            <!--상품Q&A내역 끝-->
+          </div>
+        </div>
 
         <!--mypage 끝-->
         <!-- 푸터시작 -->
@@ -273,7 +319,92 @@ li {
     font-weight: 700;
     letter-spacing:-.04em;
 }
-/* 로그인 끝 */
+.mypage_wrap {
+  margin: 40px auto;
+}
+.mypage_container {
+
+}
+.intro_wrap {
+  border: 1px solid rgba(189, 181, 181, 0.71);
+  width: 820px;
+  margin: 0 auto;
+  height: 112px;
+}
+.intro_content {
+  border-bottom: 1px solid rgba(189, 181, 181, 0.71);
+  width: 100%;
+  height: 55px;
+  background-color: #cfd31a;
+}
+.intro_tit {
+  width: 90%;
+  margin: 0 auto;
+  line-height: 45px;
+  transform: translateY(-6px);
+
+}
+.mypage_profile {
+  border-radius: 100%;
+  width: 40px;
+  height: 40px;
+  transform: translateY(13px);
+
+}
+.intro_tit span {
+
+}
+.intro_tit span:nth-child(1) {
+}
+.intro_tit span:nth-child(2) {
+  font-size: 20px;
+  margin-left: 10px;
+  color: white;
+  font-weight: 900;
+}
+.intro_tit span:nth-child(3) {
+  margin-left: 10px;
+  color: white;
+  float: right;
+  transform: translateY(10px);
+
+}
+.intro_tit span:nth-child(3) span {
+  color: white;
+  font-size: 14px;
+  font-weight: 700;
+
+}
+.intro_tit span:nth-child(3) span span{
+  color: rgba(96, 78, 78, 0.27);
+  font-size: 17px;
+}
+.point-box {
+  line-height: 55px;
+  font-size: 13px;
+  font-weight: 700;
+}
+.point-box_point {
+  margin-left: 80px;
+}
+.point-box_point span {
+  font-size: 18px;
+  color: #f27370;
+}
+.point-box_coupon {
+  margin-left: 200px;
+}
+.point-box_coupon span{
+  font-size: 18px;
+  color: #f27370;
+}
+.point-box_deposit {
+  margin-left: 180px;
+}
+.point-box_deposit span{
+  font-size: 18px;
+  color: #f27370;
+}
 </style>
 
 
